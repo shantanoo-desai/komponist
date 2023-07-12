6e336161b4d2e5561bd38a1c540d613a427148d0
Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>
2023-07-12 22:07:43 +0200

    fix(mosquitto_passwd): typo ITERATION not found
    Signed-off-by: Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>

--- filter_plugins/mosquitto_passwd.py
+++ filter_plugins/mosquitto_passwd.py
@@ -31,7 +31,7 @@ def mosquitto_passwd(passwd):
     SALT_SIZE = 12
     ITERATIONS = 101
 
-    digest = passlib.hash.pbkdf2_sha512.using(salt_size=SALT_SIZE, rounds=ITERATION) \
+    digest = passlib.hash.pbkdf2_sha512.using(salt_size=SALT_SIZE, rounds=ITERATIONS) \
                                         .hash(passwd) \
                                         .replace("pbkdf2-sha512", "7") \
                                         .replace(".", "+")
